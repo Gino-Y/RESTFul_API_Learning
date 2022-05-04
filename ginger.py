355a23c494626c8a247ffa82b64a0fafc1346f27
Gino-Y <ginoyan250@gmail.com>
2022-05-04 23:17:05 +0800

    1

--- ginger.py
+++ ginger.py
@@ -1,13 +1,6 @@
-from flask import Flask
 from app.app import create_app
 
 app = create_app()
-
-
-@app.route('/v1/user/get')
-def get_user():
-    return 'I am Gino!'
-
 
 if __name__ == '__main__':
     app.run()
