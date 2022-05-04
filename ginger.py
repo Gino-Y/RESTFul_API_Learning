1a13fb2a1ed89736d93fae3a9e692faa52c4bb6b
Gino-Y <ginoyan250@gmail.com>
2022-05-04 21:22:41 +0800

    1

--- ginger.py
+++ ginger.py
@@ -4,9 +4,9 @@ from app.app import create_app
 app = create_app()
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/v1/user/get')
+def get_user():
+    return 'I am Gino!'
 
 
 if __name__ == '__main__':
